7c090972e11f848ed84456a8252d15cf9b822471
Evgeny Paromov <evgeny.paromov@evrone.com>
2024-10-15 22:03:32 +0300

    feat: keepPrevious data

--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,13 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { todoListApi } from "./api";
 import { useState } from "react";
 
 export function TodoList() {
   const [page, setPage] = useState(1);
 
-  const { data: todoItems, error, isPending } = useQuery({
+  const { data: todoItems, error, isPending, isPlaceholderData } = useQuery({
     queryKey: ["tasks", "list", { page }],
-    queryFn: meta => todoListApi.getTodoList({ page }, meta)
+    queryFn: meta => todoListApi.getTodoList({ page }, meta),
+    placeholderData: keepPreviousData
   });
 
   if (isPending) {
@@ -22,7 +23,7 @@ export function TodoList() {
     <div className="p-5 mx-auto max-w-[1200px] mt-10">
       <h1 className="text-3xl font-bold underline mb-5"> Todo LIst</h1>
 
-      <div className="flex flex-col gap-4">
+      <div className={"flex flex-col gap-4" + (isPlaceholderData ? ' opacity-50': '')}>
         {todoItems.data.map(todo => (
           <div className="border border-slate-300 rounded p-3" key={todo.id}>
             {todo.text}
