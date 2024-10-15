221ab8e4b356897507d53304bdfe890dd0b91aae
Evgeny Paromov <evgeny.paromov@evrone.com>
2024-10-15 21:48:51 +0300

    feat: stale time

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,13 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../shared/api/query-client";
 import { TodoList } from "../modules/todo-list/todo-list";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TodoList/>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
