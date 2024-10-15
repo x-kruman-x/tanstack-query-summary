5545e1557a304533e182c2cc9121a3953768442f
Evgeny Paromov <evgeny.paromov@evrone.com>
2024-10-15 21:19:40 +0300

    feat: add base styles

--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { todoListApi } from "./api";
 
-
 export function TodoList() {
   const { data, error, isPending } = useQuery({
     queryKey: ["tasks", "list"],
@@ -17,11 +16,14 @@ export function TodoList() {
   }
 
   return (
-    <div>
-      Todo List
-      {data.map(todo => (
-        <div key={todo.id}> {todo.text} </div>
-      ))}
+    <div className="p-5 mx-auto max-w-[1200px] mt-10">
+      <h1 className="text-3xl font-bold underline mb-5"> Todo LIst</h1>
+
+      <div className="flex flex-col gap-4">
+        {data.map(todo => (
+          <div className="border border-slate-300 rounded p-3" key={todo.id}> {todo.text} </div>
+        ))}
+      </div>
     </div>
   );
 }
