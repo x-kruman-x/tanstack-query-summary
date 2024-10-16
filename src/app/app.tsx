01e0eec6b07ad0f64b3db331937350ec7820bb9f
Evgeny Paromov <evgeny.paromov@evrone.com>
2024-10-16 15:45:07 +0300

    feat: add redux

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,16 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../shared/api/query-client";
 import { TodoList } from "../modules/todo-list/todo-list";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Provider } from "react-redux";
+import { store } from "../shared/redux";
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TodoList/>
+      <Provider store={store}>
+        <TodoList />
+      </Provider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
---
+++ src/shared/redux.ts
@@ -0,0 +1,38 @@
+import {
+    combineSlices,
+    createSelector,
+    ThunkAction,
+    UnknownAction,
+  } from "@reduxjs/toolkit";
+  import { useDispatch, useSelector, useStore } from "react-redux";
+  import { configureStore } from "@reduxjs/toolkit";
+
+  
+  export const rootReducer = combineSlices();
+  
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export type AppState = any;
+  export type AppDispatch = typeof store.dispatch;
+  export type AppThunk<R = void> = ThunkAction<
+    R,
+    AppState,
+    {},
+    UnknownAction
+  >;
+
+
+
+export const store = configureStore({
+  reducer: rootReducer,
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware(),
+});
+
+  
+  export const useAppSelector = useSelector.withTypes<AppState>();
+  export const useAppDispath = useDispatch.withTypes<AppDispatch>();
+  export const useAppStore = useStore.withTypes<typeof store>();
+  export const createAppSelector = createSelector.withTypes<AppState>();
+
+  
