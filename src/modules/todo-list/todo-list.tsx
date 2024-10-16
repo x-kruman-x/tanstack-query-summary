fd9f6f1ba2ee55fad3951ffa6e09acbd66c6aa93
Evgeny Paromov <evgeny.paromov@evrone.com>
2024-10-16 12:02:42 +0300

    feat: infinity scroll

--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,17 +1,34 @@
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import {
+  keepPreviousData,
+  useInfiniteQuery,
+  useQuery
+} from "@tanstack/react-query";
 import { todoListApi } from "./api";
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
 
 export function TodoList() {
-  const [page, setPage] = useState(1);
+  const [enabled, setEnable] = useState(false);
 
-  const [enabled, setEnable] = useState(false)
+  const {
+    data: todoItems,
+    error,
+    isLoading,
+    isPlaceholderData,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage
+  } = useInfiniteQuery({
+    queryKey: ["tasks", "list"],
+    queryFn: meta => todoListApi.getTodoList({ page: meta.pageParam }, meta),
+    enabled: enabled,
+    initialPageParam: 1,
+    getNextPageParam: result => result.next,
+    select: result => result.pages.flatMap(page => page.data)
+  });
 
-  const { data: todoItems, error,  isLoading, status, fetchStatus, isPlaceholderData } = useQuery({
-    queryKey: ["tasks", "list", { page }],
-    queryFn: meta => todoListApi.getTodoList({ page }, meta),
-    placeholderData: keepPreviousData,
-    enabled: enabled
+  
+  const cursorRef = useIntersection(() => {
+    fetchNextPage();
   });
 
   if (isLoading) {
@@ -22,32 +39,48 @@ export function TodoList() {
     return <div>error: {JSON.stringify(error)}</div>;
   }
 
+
   return (
     <div className="p-5 mx-auto max-w-[1200px] mt-10">
       <h1 className="text-3xl font-bold underline mb-5"> Todo LIst</h1>
       <button onClick={() => setEnable(e => !e)}>Toggle enabled</button>
 
-      <div className={"flex flex-col gap-4" + (isPlaceholderData ? ' opacity-50': '')}>
-        {todoItems?.data.map(todo => (
+      <div
+        className={
+          "flex flex-col gap-4" + (isPlaceholderData ? " opacity-50" : "")
+        }
+      >
+        {todoItems?.map(todo => (
           <div className="border border-slate-300 rounded p-3" key={todo.id}>
             {todo.text}
           </div>
         ))}
       </div>
-      <div className="flex gap-2 mt-4">
-        <button
-          onClick={() => setPage(p => Math.max(p - 1, 1))}
-          className="p-3 rounded border border-teal-500"
-        >
-          prev
-        </button>
-        <button
-          onClick={() => setPage(p => Math.min(p + 1, todoItems?.pages ?? 1))}
-          className="p-3 rounded border border-teal-500"
-        >
-          next
-        </button>
+      <div className="flex gap-2 mt-4" ref={cursorRef}>
+        {!hasNextPage && <div>Нет данных для загрузки </div>}
+        {isFetchingNextPage && <div>...Loading</div>}
       </div>
     </div>
   );
+}
+
+export function useIntersection(onIntersect: () => void) {
+  const unsubscribe = useRef(() => {});
+
+  return useCallback((el: HTMLDivElement | null) => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(intersection => {
+        if (intersection.isIntersecting) {
+          onIntersect();
+        }
+      });
+    });
+
+    if (el) {
+      observer.observe(el);
+      unsubscribe.current = () => observer.disconnect();
+    } else {
+      unsubscribe.current();
+    }
+  }, []);
 }
